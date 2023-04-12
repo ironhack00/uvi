cc5f14cd4ce38efb6ac5bc3b4db2e9ceeb2cf82a
ironhack00 <guille.fernandeeez@gmail.com>
2023-04-12 15:42:27 -0400

    vamos

--- src/Component/Form/Form.jsx
+++ src/Component/Form/Form.jsx
@@ -1,6 +1,7 @@
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import styles from './Form.module.css'
+import Carousel from 'react-bootstrap/Carousel';
 
 function Formm() {
   return (
@@ -23,6 +24,31 @@ function Formm() {
       <Button variant="primary" type="submit">
         Submit
       </Button>
+
+      {/* carousel */}
+      <Carousel>
+      <Carousel.Item className={styles.img}>
+        <img
+          className="d-block w-100"
+          src="https://images.unsplash.com/photo-1612372992697-81c95501d0d1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+          alt="First slide"
+        />
+      </Carousel.Item>
+      <Carousel.Item className={styles.img}>
+        <img
+          className="d-block w-100"
+          src="https://images.unsplash.com/photo-1601625463687-25541fb72f62?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+          alt="Second slide"
+        />
+      </Carousel.Item>
+      <Carousel.Item className={styles.img}>
+        <img
+          className="d-block w-100"
+          src="https://images.unsplash.com/photo-1578172336697-11f964fe6b7a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+          alt="Third slide"
+        />
+      </Carousel.Item>
+    </Carousel>
     </Form>
   );
 }
