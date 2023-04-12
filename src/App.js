0ccbdd6834d79358191af41bbb27b41fc30538c8
ironhack00 <guille.fernandeeez@gmail.com>
2023-04-12 15:47:50 -0400

    vamooo

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import NavBar from "./Component/NavBar/NavBar";
 import Carousel1 from "./Component/Carousel/Carousel";
 import BorderExample from './Component/Planes/Planes.jsx';
 import Formm from "./Component/Form/Form";
+import Footer from "./Component/Footer/Footer";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Carousel1/>
       <BorderExample/>
       <Formm/>
+      <Footer/>
     </>
   );
 }
